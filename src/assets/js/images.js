ef39fe94d863b9fe0875d533934b589ca6d06079
grafDev <gregory.yakovlev@gmail.com>
2025-08-07 11:57:18 +0200

    fix images loading

--- src/assets/js/images.js
+++ src/assets/js/images.js
@@ -1,3 +1,18 @@
+// Import all possible images
+import textWheelImg from '../images/text_wheel.png';
+import textWheelCanadaImg from '../images/text_wheel-canada.png';
+import modalBgImg from '../images/modal_bg.png';
+import modalBgCanadaButtonImg from '../images/modal_bg-canada-button.png';
+import modalBgCanadaAutoImg from '../images/modal_bg-canada-auto.png';
+
+const imageMap = {
+    'text_wheel.png': textWheelImg,
+    'text_wheel-canada.png': textWheelCanadaImg,
+    'modal_bg.png': modalBgImg,
+    'modal_bg-canada-button.png': modalBgCanadaButtonImg,
+    'modal_bg-canada-auto.png': modalBgCanadaAutoImg
+};
+
 export function getImagePath(filename) {
-    return new URL(`../images/${filename}`, import.meta.url).href;
+    return imageMap[filename] || new URL(`../images/${filename}`, import.meta.url).href;
 }
